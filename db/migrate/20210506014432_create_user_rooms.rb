819b4965e31d15cd190bbca9d32135352ed505ee
sugiryota <83449662+sugiryota@users.noreply.github.com>
2021-05-06 11:03:18 +0900

    room_usersテーブルの作成

---
+++ db/migrate/20210506014432_create_user_rooms.rb
@@ -0,0 +1,10 @@
+class CreateUserRooms < ActiveRecord::Migration[6.0]
+  def change
+    create_table :user_rooms do |t|
+      t.references :room, foreign_key:true
+      t.references :user, foreign_key:true
+
+      t.timestamps
+    end
+  end
+end
