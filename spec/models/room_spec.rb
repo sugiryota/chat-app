fd88fce0d1e54672ff8259278ece91cebe4f749c
sugiryota <83449662+sugiryota@users.noreply.github.com>
2021-05-07 14:48:43 +0900

    各モデルのテスト

---
+++ spec/models/room_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe Room, type: :model do
+  describe '#create' do
+    before do
+      @room = FactoryBot.build(:room)
+    end
+
+    it 'nameの値が存在すれば登録できること' do
+      expect(@room).to be_valid
+
+
+
+
+    end
+
+    it 'nameが空では登録できないこと' do
+      @user.name=''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Name can't be blank")
+
+    end
+  end
+end
